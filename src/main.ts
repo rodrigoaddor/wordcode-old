5339517b9870b077bb1215f775dd480894c655c8
Rodrigo Addor <rodrigotladdor@gmail.com>
2019-06-04 21:30:04 -0300

    Project Setup

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import Menu from './screen/menu'
+
+ReactDOM.render(
+  React.createElement(Menu),
+  document.getElementById('app')
+)
---
+++ src/screen/menu.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+import './menu.sass'
+
+class MenuScreen extends React.Component {
+  render() {
+    return (
+      <div className='screen'>
+        <h1 className='is-title'>WordCode</h1>
+      </div>
+    )
+  }
+}
+
+export default MenuScreen
